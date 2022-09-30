52eeeacb83633a93ce4c63d07b8402aae1a83bde
Ismael Sabri <hisme14@gmail.com>
2022-09-30 18:42:34 +0200

    Added test for map and detail classes

---
+++ IOSAvanzadoDragonBallTests/Test Classes/MapViewControllerSpy.swift
@@ -0,0 +1,8 @@
+//
+//  MapViewControllerSpy.swift
+//  IOSAvanzadoDragonBallTests
+//
+//  Created by Ismael Sabri Pérez on 29/9/22.
+//
+
+import Foundation
---
+++ IOSAvanzadoDragonBallTests/Test Classes/MapViewModelSpy.swift
@@ -0,0 +1,8 @@
+//
+//  MapViewModelSpy.swift
+//  IOSAvanzadoDragonBallTests
+//
+//  Created by Ismael Sabri Pérez on 28/9/22.
+//
+
+import Foundation
